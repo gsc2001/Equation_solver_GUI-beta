73cfd3581412fb25e85f71ca9d5680439c21444c
gsc2001 <gurkiratsingh2001@gmail.com>
2020-03-21 21:52:27 +0530

    added comment in main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from testing import get_solve
 from interface import App
 
-root = App('Equation Solver',(1000,700),'Segoe 15')
+root = App('Equation Solver',(1000,700),'Segoe 15')     # change sizeof window and text font/font size here
 root.start()
